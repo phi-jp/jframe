109c15c5cdc458c4fdf4f58c3c548c8af9afb178
phi <phi1618jp@gmail.com>
2016-04-25 23:35:12 +0900

    add src

---
+++ src/jframe.js
@@ -0,0 +1,82 @@
+
+
+;(function(global) {
+  
+  /**
+   * 
+   */
+  var jframe = function(query) {
+    this.init(query);
+    return this;
+  };
+
+  jframe.prototype = {
+
+    /**
+     * 
+     */
+    init: function(query) {
+      this.domElement = document.querySelector(query);
+      this.domElement.classList.add('jframe');
+      this.load('');
+
+      this.attributes = {};
+    },
+
+    attr: function(key, value) {
+      this.attributes[key] = value;
+    },
+
+    /**
+     * 
+     */
+    load: function(code) {
+      var iframe = document.createElement("iframe");
+
+      for (var key in this.attributes) {
+        var v = this.attributes[key];
+        iframe.setAttribute(key, v);
+      }
+
+      this.domElement.innerHTML = "";
+      this.domElement.appendChild(iframe);
+
+      // for safari(first value becomes 0 for some reason)
+      iframe.contentWindow.innerWidth = window.innerWidth;
+      iframe.contentWindow.innerHeight = window.innerHeight;
+
+      // フレーム
+      var idoc = iframe.contentDocument;
+      idoc.open();
+      idoc.write(code);
+      idoc.close();
+
+      return this;
+    },
+
+    /**
+     * 
+     */
+    reload: function() {
+      var iframe = this.domElement.querySelector("iframe");
+
+      iframe.contentWindow.location.reload();
+
+      return this;
+    },
+
+    getFrame: function() {
+      return this.domElement.children[0];
+    },
+
+    getDocument: function() {
+      return this.getFrame().contentDocument;
+    },
+  };
+
+  global.jframe = function(q) {
+    return new jframe(q);
+  };
+
+})(this);
+
